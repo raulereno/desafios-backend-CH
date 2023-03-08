a4096be1e3b9eb01caee092ed0efebd3fb92714f
Raul Ereño <raulereno@gmail.com>
2023-03-08 09:53:41 -0300

    eliminando version vieja de segunda preetrega

--- 2da_preentrega/src/helpers/total_count.js
+++
@@ -1,8 +0,0 @@
-module.exports = totalCount = (products) => {
-  console.log(products);
-  let sum = 0;
-  products.forEach((product) => {
-    sum += product.product.price * product.quantity;
-  });
-  return sum.toFixed(2);
-};
