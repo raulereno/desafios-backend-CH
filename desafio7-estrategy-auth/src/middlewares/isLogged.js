e2499d5bda646ea114b3ca348001a0bdaeb83117
Raul Ereño <raulereno@gmail.com>
2023-03-19 14:41:42 -0300

    solucionando error de logeo

--- desafio7-estrategy-auth/src/middlewares/isLogged.js
+++ desafio7-estrategy-auth/src/middlewares/isLogged.js
@@ -2,8 +2,7 @@ const { getUserByUsername } = require("../services/user.service");
 
 const isLogged = async (req, res, next) => {
   const user = await getUserByUsername(req.session?.user.username);
-
-  if (user || req.session?.user.email === process.env.ADMIN_USER) {
+  if (user || req.session.user?.email === process.env.ADMIN_USER) {
     //Genera un bucle
     // if (["/login", "/register"].includes(req.url)) {
     //   res.redirect("/");
