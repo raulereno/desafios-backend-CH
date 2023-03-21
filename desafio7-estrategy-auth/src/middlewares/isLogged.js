84fbe31d57652564b7a59b712cdcc0360eb6190c
Raul Ereño <raulereno@gmail.com>
2023-03-21 14:49:09 -0300

    fix  TypeError: Cannot read properties of undefined (reading 'username') when the user try to access to /products without login first

--- desafio7-estrategy-auth/src/middlewares/isLogged.js
+++ desafio7-estrategy-auth/src/middlewares/isLogged.js
@@ -1,7 +1,7 @@
 const { getUserByUsername } = require("../services/user.service");
 
 const isLogged = async (req, res, next) => {
-  const user = await getUserByUsername(req.session?.user.username);
+  const user = await getUserByUsername(req.session?.user?.username);
   if (user || req.session.user?.email === process.env.ADMIN_USER) {
     //Genera un bucle
     // if (["/login", "/register"].includes(req.url)) {
